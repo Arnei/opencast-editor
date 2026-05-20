a9d5cd987eb404780c6863862b934927743baba7
Arnei <arnewilken@yahoo.de>
2026-05-20 16:22:13 +0200

    Fix missed changes for vitejs/plugin-react update

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import child from "child_process";
 import { configDefaults } from 'vitest/config'
+import babel from '@rolldown/plugin-babel'
 
 const commitHash = child.execSync("git rev-parse HEAD").toString().trim();
 
@@ -19,9 +20,9 @@ export default defineConfig(() => {
     plugins: [
       react({
         jsxImportSource: "@emotion/react",
-        babel: {
-          plugins: ["@emotion/babel-plugin"],
-        },
+      }),
+      babel({
+        plugins: ["@emotion/babel-plugin"],
       }),
       // svgr options: https://react-svgr.com/docs/options/
       svgr({ svgrOptions: { } }),
